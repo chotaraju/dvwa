6ff6fb0d74a065bc1180d0349145dcdc4f0009e8
chotaraju <115630656+chotaraju@users.noreply.github.com>
2024-04-23 22:41:21 +0530

    Add files via upload

---
+++ 1.sol
@@ -0,0 +1,18 @@
+// a Smart contract to set and get a message
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.17; 
+contract Message {
+    string message;
+    
+    constructor (string memory newMessage) public { 
+        message = newMessage;
+    } 
+    
+    function setMessage(string memory newMessage) public { 
+        message = newMessage;
+    } 
+    
+    function getMessage() public view returns(string memory) { 
+        return message;
+    } 
+}
